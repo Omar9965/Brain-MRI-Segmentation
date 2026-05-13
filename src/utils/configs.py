a9f38598c02c89e88a26c19b5c0a54de40e86e96
Omar <eng16.omarmohamed@gmail.com>
2026-05-13 22:54:07 +0300

    feat: add environment configuration and Pydantic settings management

--- src/utils/configs.py
+++ src/utils/configs.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    APP_NAME: str
-    APP_VERSION: str
+    APP_NAME: str = "brain-mri-segmentation"
+    APP_VERSION: str = "1.0.0"
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
